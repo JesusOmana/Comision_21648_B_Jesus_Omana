f5c09b520abad94f3cdf6277b3cc452f903fb2fe
jesus omana <jesus.omanamarquez@gmail.com>
2023-09-16 23:47:44 -0300

    se modifico el servidor y se estudio los siguientes pasos para empezar a trabajar en en view

--- servidor.js
+++ servidor.js
@@ -9,19 +9,19 @@ const { sequelize } = require("./database")
 app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
-
 app.use(express.static("public"))
-
 app.set("view engine", "ejs")
 
-app.use(require("./src/routes/tareas.routes"))
 
+/* routes! */
+app.use(require("./src/routes/tareas.routes"))
+app.set("views", __dirname,("/view"))
 
 
 app.listen(3000, () => {
     /* se cambio de authenticate a SYNC para que me cree
     la tabla en caso de que no exista */
-    sequelize.sync({ force: false });
+    sequelize.sync({ force: true });
 
     console.log("servidor 3000")
 })
